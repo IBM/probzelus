6e85fbcd8570a379eb2c80748212127ef5e69136
Louis Mandel <lmandel@us.ibm.com>
2020-06-29 12:35:21 -0400

    New SLAM map

--- examples/slam-jfla/map.ml
+++ examples/slam-jfla/map.ml
@@ -33,6 +33,38 @@ let pldi =
     [ false; true; false; false; false; false; true; true; true; false; true; true; true; false; false; true; true; true; false; ];
   ]
 
+
+
+ (* X   X XXX XXX  *)
+ (* XX XX  X   X   *)
+ (* X X X  X   X   *)
+ (* X   X  X   X   *)
+ (* X   X XXX  X   *)
+ (*                *)
+ (* XXX XXX  X   X *)
+ (*  X  X  X XX XX *)
+ (*  X  XXX  X X X *)
+ (*  X  X  X X   X *)
+ (* XXX XXX  X   X *)
+
+let mit_ibm =
+  [
+    [ false; true;  false; false; false; true;  false; true;  true;  true;  false; true;  true;  true;  false; false; ];
+    [ false; true;  true;  false; true;  true;  false; false; true;  false; false; false; true;  false; false; false; ];
+    [ false; true;  false; true;  false; true;  false; false; true;  false; false; false; true;  false; false; false; ];
+    [ false; true;  false; false; false; true;  false; false; true;  false; false; false; true;  false; false; false; ];
+    [ false; true;  false; false; false; true;  false; true;  true;  true;  false; false; true;  false; false; false; ];
+    [ false; false; false; false; false; false; false; false; false; false; false; false; false; false; false; false; ];
+    [ false; true;  true;  true;  false; true;  true;  true;  false; false; true;  false; false; false; true;  false; ];
+    [ false; false; true;  false; false; true;  false; false; true;  false; true;  true;  false; true;  true;  false; ];
+    [ false; false; true;  false; false; true;  true;  true;  false; false; true;  false; true;  false; true;  false; ];
+    [ false; false; true;  false; false; true;  false; false; true;  false; true;  false; false; false; true;  false; ];
+    [ false; true;  true;  true;  false; true;  true;  true;  false; false; true;  false; false; false; true;  false; ];
+  ]
+
+
+(* ------------------------- *)
+
 let rec t m =
   begin match m with
   | [] -> []
@@ -45,6 +77,7 @@ let rec t m =
   end
 
 let tm =
-  t pldi
+  t mit_ibm
 
 let _ = List.length tm;;
+let _ = List.length (List.hd tm);;
