e3c52ae28b51525baa73219170fbe41611779f2e
Ellie Cheng <ellieyhc@csail.mit.edu>
2023-06-22 15:59:42 -0400

    Swap with parents bugfix
    Signed-off-by: Ellie Cheng <ellieyhc@csail.mit.edu>

--- probzelus/semi_symbolic/src/semi_symbolic_impl.ml
+++ probzelus/semi_symbolic/src/semi_symbolic_impl.ml
@@ -1512,7 +1512,7 @@ fun rv ->
             (if swap rv_par rv_current then record_approx_status rv_par.name (Exact 1)
             else raise (NonConjugate rv_par));
             swap_with_parents rest
-          ) else ())
+          ) else swap_with_parents rest)
         end
       in
       swap_with_parents (List.rev parents);
