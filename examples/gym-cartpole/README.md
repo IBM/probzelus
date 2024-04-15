774e77557265d20c9738407bea8464c197127174
VictorBlanchi <76261859+VictorBlanchi@users.noreply.github.com>
2024-04-15 11:13:33 +0200

    Gymnasium (#14)
    * Migrate to Gymnasium
    
    Signed-off-by: Victor Blanchi <victor.blanchi@ens.psl.eu>
    
    * Close envs on signal SIGINT
    
    Signed-off-by: Victor Blanchi <victor.blanchi@ens.psl.eu>
    
    ---------
    
    Signed-off-by: Victor Blanchi <victor.blanchi@ens.psl.eu>
    Co-authored-by: Guillaume Baudart <guillaume.baudart@inria.fr>

--- examples/gym-cartpole/README.md
+++ examples/gym-cartpole/README.md
@@ -1,31 +1,15 @@
-To compile and execute this example, you need [gym-http-api](https://github.com/openai/gym-http-api), the [openai-gym](https://github.com/openai/gym) REST API and [openai-gym-ocaml](https://github.com/IBM/openai-gym-ocaml), its OCaml binding.
+To compile and execute this example, you need [pyml](https://github.com/thierry-martinez/pyml) OCaml bindings for Pythom and [gymnasium](https://gymnasium.farama.org/index.html)
+The (Python) Gymnasium package is called from OCaml via pyml.
 
-The gym-http-api can be installed as follows:
-
-```
-cd ~
-git clone https://github.com/openai/gym-http-api
-cd gym-http-api
-cat << EOF > requirements.txt
-Flask==0.12.4
-numpy==1.16.3
-gym==0.7.4
-requests==2.12.4
-pytest
-EOF
-pip install -r requirements.txt
+The gymnasium package can be installed as follows.
+``` shell
+pip install gymnasium opencv-python pygame
 ```
 
 The Ocaml binding can be installed as follows:
 
-```
-opam install openai-gym
-```
-
-To execute this example, you first have to launch the openai-gym:
-
-```
-python $PATH_TO_gym-http-api/gym_http_server.py &
+``` shell
+opam install pyml
 ```
 
 Then the example can be executed from `~/probzelus/examples/gym-cartpole` with
---
+++ examples/gymnasium/utils.ml
@@ -0,0 +1,57 @@
+(** This type is used to define the render mode in gymnasium. 
+    Human render does not allow multi-windows. Use Human_multi instead (less efficient) *)
+type render = Human | Human_multi | Ascii | Rgb_array
+
+let () =
+  Py.add_python_path "../gymnasium/python_src" (* TODO: improve this code *)
+
+let () = Py.initialize ()
+let gym = Py.import "gymnasium"
+let render_multi = Py.import "rendermulti"
+
+let render_to_str (render : render) : string =
+  match render with
+  | Human -> "human"
+  | Ascii -> "ascii"
+  | Rgb_array | Human_multi -> "rgb_array"
+
+(** Create a fresh windows name *)
+let fresh_name : string -> string =
+  let j = ref 0 in
+  function
+  | name ->
+      j := !j + 1;
+      String.concat "" [ name; string_of_int !j ]
+
+(** Create a new gym environment, using by default a human render. *)
+let make ~render game =
+  let env =
+    Py.Module.get_function_with_keywords gym "make"
+      [| Py.String.of_string game |]
+      [ ("render_mode", Py.String.of_string @@ render_to_str render) ]
+  in
+  match render with
+  | Human | Ascii | Rgb_array -> env
+  | Human_multi ->
+      Py.Module.get_function_with_keywords render_multi "RenderMultiHuman"
+        [| env; Py.String.of_string @@ fresh_name game |]
+        []
+
+let reset env = Py.Object.call_method env "reset" [||]
+
+let step env action =
+  let next = Py.Object.call_method env "step" [| action |] in
+  let observation, reward, terminated, truncated, info =
+    Py.Tuple.to_tuple5 next
+  in
+  ( observation,
+    reward,
+    Py.Bool.to_bool terminated,
+    Py.Bool.to_bool truncated,
+    info )
+
+let close env = ignore @@ Py.Object.call_method env "close" [||]
+
+let sample_action env =
+  let action_space = Py.Object.find_attr_string env "action_space" in
+  Py.Object.call_method action_space "sample" [||]
